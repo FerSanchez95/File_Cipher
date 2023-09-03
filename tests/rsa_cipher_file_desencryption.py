03ead5009aeac450a6c80281b3ae7bfbe3e00583
fernando <fersanchezz95@gmail.com>
2023-09-03 13:16:49 -0300

    Formateo de código. Comentarios y código excedente eliminados.

--- tests/rsa_cipher_file_desencryption.py
+++ tests/rsa_cipher_file_desencryption.py
@@ -9,51 +9,31 @@ class de_encryptor:
         self.cipher_text = file_to_decrypt
 
     def file_decipher(self):
-        #Construyo la ruta.
+
         self.path_to_key = os.getcwd()
         self.private_key_path = str(self.path_to_key + "/" + self.private_key)
         self.path_to_cipher_file = str(self.path_to_key + "/" + self.cipher_text)
 
-        #Ninguno de los 3 bloques escritos funciona.
-
-        #Bloque para el descifrado sin sentencia "with".
-        self.instance_for_private_key = open(self.private_key_path, "rb").read() #Creo una instancia de apertura para la llave con la que voy a descifrar
+        self.instance_for_private_key = open(self.private_key_path, "rb").read() 
+        self.instance_for_encrypted_data = open(self.path_to_cipher_file, "rb").read()
         self.load_private_key = RSA.import_key(self.instance_for_private_key)
         self.loaded_private_key = PKCS1_OAEP.new(self.load_private_key)
-        self.instance_for_encrypted_data = open(self.path_to_cipher_file, "rb").read() #Creo una instancia de apertura para el archivo que voy a descifrar
         self.decipher_text = self.loaded_private_key.decrypt(self.instance_for_encrypted_data)
-        print(self.decipher_text) #.decode("utf-8")
+        print(self.decipher_text)
 
         with open(self.path_to_cipher_file, "wb") as self.f:
             self.f.write(self.decipher_text)
             self.f.close()
 
-        #Bloque con apertura del archivo de la llave privada.
-        #with open(self.private_key_path, "rb") as self.pk:
-        #    self.load_private_key = RSA.import_key(self.pk.read())
-        #    self.loaded_private_key = PKCS1_OAEP.new(self.load_private_key)
-        #    self.decipher_text = self.loaded_private_key.decrypt(self.cipher_text)
-        #    print(self.decipher_text.decode("utf-8"))
-        #    self.pk.close()
-
-        #Bloque con apertura del archivo de texto cifrado.    
-        #with open(self.cipher_text, "rb") as self.cf: # wb = write, binary mode; rb = read, binary mode; wrb = write, read, binary mode. 
-        #     #Cargo la llave privada 
-        #    self.private_key = RSA.import_key(open(self.private_key_path).read())
-        #    self.key = PKCS1_OAEP.new(self.private_key)
-        #    self.decipher_text = self.key.decrypt(self.cf)
-        #    self.cf.write(self.decipher_text)
-        #    self.cf.close()
-        return print(f"\nArchivo descifrado en: {self.path_to_cipher_file}")
-        #PROBAR SI DESCIFRA Y GUARDA EL CONTENIDO DEL ARCHIVO!
+        return print(f"\n === Archivo descifrado en: {self.path_to_cipher_file} ===\n")
     
     def __call__(self):
         self.file_decipher()
 
 def main():
 
-    password_bytes = input('Ingrese el nombre del archivo que contiene la contraseña: ')
-    plain_text_file = input("Escriba el nombre del archivo que desea descifrar: ")
+    password_bytes = input(f"\n >> Ingrese el nombre del archivo que contiene la contraseña: ")
+    plain_text_file = input(" >> Escriba el nombre del archivo que desea descifrar: ")
     new_deencripted_file = de_encryptor(password_bytes, plain_text_file)
     new_deencripted_file()
 
--- tests/rsa_cipher_file_encryption.py
+++ tests/rsa_cipher_file_encryption.py
@@ -11,7 +11,6 @@ class encryptor:
     def key_generator(self): 
         self.new_key = RSA.generate(self.key_lenght)
         self.public_key_file = open('public_key.pem', 'wb')
-        #¿Es necesario que el archivo tenga la extención '.PEM'?
         self.new_public_key = self.new_key.publickey().export_key('DER')
         self.public_key_file.write(self.new_public_key)
         self.public_key_file.close()
@@ -24,54 +23,25 @@ class encryptor:
 
     def file_cipher(self):
         self.folder_path = os.getcwd()
-        self.file_path = self.folder_path + "/" + self.file #Completar la ruta del archivo.
+        self.file_path = self.folder_path + "/" + self.file 
         self.f = open(self.file_path).read() 
-        self.data_to_encrypt = self.f.encode("utf-8")  #Codificación UTF-8 del los datos del archivo.         
+        self.data_to_encrypt = self.f.encode("utf-8") 
         self.key_to_encrypt = self.key_generator() 
         self.cipher = PKCS1_OAEP.new(self.key_to_encrypt.publickey())
         self.encrypted_data = self.cipher.encrypt(self.data_to_encrypt)
+
         with open(self.file_path, "wb") as self.f:
             self.f.write(self.encrypted_data)
             self.f.close()
             
-        return print(f"\n=== Archivo cifrado en {self.folder_path} ===\n")
-
-    # def file_cipher(self):
-    #     self.folder_path = os.getcwd()
-    #     self.file_path = self.folder_path + "/" + self.file #Completar la ruta del archivo.
-    #     with open(self.file_path, "rb") as self.f:   #Abrir el archivo y encriptarlo con las llaves generadas.
-    #         self.read_data = self.f.read() #Lecturad del archivo
-    #         self.encoded_data = self.read_data.encode("utf-8")  #Codificación UTF-8 del los datos del archivo.         
-    #         self.key_to_encrypt = self.key_generator() #Llamo al generador de llaves
-    #         self.cipher = PKCS1_OAEP.new(self.key_to_encrypt.publickey())
-    #         #Los datos a cifrar deben estar codificados en formato UTF-8 antes de encriptarlos. 
-    #         self.text_to_cipher = self.cipher.encrypt(self.encoded_data)
-    #         self.f.close()
-    #     with open(self.file_path, "wb") as self.f:
-    #         self.f.write(self.text_to_cipher)
-    #         self.f.close()
-            
-    #     return print(f"\n=== Archivo cifrado en {self.folder_path} ===\n")
-
-    # def file_cipher(self):
-    #     self.folder_path = os.getcwd()
-    #     self.file_path = self.folder_path + "/" + self.file #Completar la ruta del archivo.
-    #     with open(self.file_path, "wb") as self.f:   #Abrir el archivo y encriptarlo con las llaves generadas.
-    #         self.key_to_encrypt = self.key_generator()
-    #         self.cipher = PKCS1_OAEP.new(self.key_to_encrypt.publickey())
-    #         # Los datos a cifrar deben estar codificados en formato UTF-8 antes de encriptarlos.
-    #         self.encoded_data = self.f.read().encode("utf-8")
-    #         self.text_to_cipher = self.cipher.encrypt(self.encoded_data.encode("utf-8"))
-    #         self.f.write(self.text_to_cipher)
-    #         self.f.close()
-    #     return print(f"\n=== Archivo cifrado en {self.folder_path} ===\n")
-
+        return print(f"\n === Archivo cifrado en {self.folder_path} ===\n")
+    
     def __call__(self):
         self.file_cipher()
 
 def main():
-    password_bytes = input('Ingrese el largo de la contraseña: ')
-    plain_text_file = input("Escriba el nombre del archivo que desea cifrar: ")
+    password_bytes = input(f"\n >> Ingrese el largo de la contraseña: ")
+    plain_text_file = input(" >> Escriba el nombre del archivo que desea cifrar: ")
     new_encripted_file = encryptor(password_bytes, plain_text_file)
     new_encripted_file()
 
