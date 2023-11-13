7f6e46457afa4aa8ed08d2280bbbc0a1c8713a35
fernando <fersanchezz95@gmail.com>
2023-11-12 22:23:19 -0300

    First commit, add README file to main branch.

--- resources/__ini__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from flask import Flask 
-
-def create_app(test_config=NONE):
-    app = Flask(__name__, istance_relative_config = True)
-    app.config.from.mapping( SECRET_KEY = 'lallavesecreta', DATABASE = os.path.join(app.instance_path, 'flask.sqlite'),)
-
