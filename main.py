79fde9f03a4a136b8922a2ed5ce350a90b392d70
FerSanchez95 <fersanchezz95@gmail.com>
2024-02-11 20:55:53 -0300

    Changes made in imports in all modules.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+'''Main. Here is maion program that execute the behaviour of the page and calls
+   for the other modules.'''
 import os
 from flask import Flask, request, flash
 from flask import render_template
 from flaskwebgui import FlaskUI # import FlaskUI
-from RSA_AES_file_dec import AesDecryptor as Decryption
-from RSA_AES_file_Enc import AesEncryptor as Encryption 
-from path_to_file_contruct import Constructor
+import modules.rsa_aes_dec as decryption
+import modules.rsa_aes_enc as encryption
+import modules.path_to_file_contruct as constructor
+
 
 app = Flask(__name__)
 app.secret_key = b'=$%$)JrwerweTW)#"fdosejr=(3)'
-ui = FlaskUI(app)
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
@@ -27,32 +29,32 @@ def cipher():
     for the encryption. 
     '''
     if request.method == 'GET':
-      new_message = f'Ingrese el archivo que desea cifrar y el número de bytes para la llave de cifrado.'
-      flash(new_message)
-      
+        message = 'Ingrese el archivo que desea cifrar y los bytes para la llave de cifrado.'
+        flash(message)
+
     if request.method == 'POST':
         try:
             file_name = request.form['file_path']
             bytes_for_key = int(request.form['bytes_long'])
-            create_new = Constructor(file_name, None)
-            path_to_cipher_file = create_new.FilePath()
-            
+            create_new = constructor.Constructor(file_name, None)
+            path_to_cipher_file = create_new.file_path()
+
             #if the file doesn't exist send error message.
             if not os.access(path_to_cipher_file, os.F_OK):
                 raise FileNotFoundError
-        
+
         except FileNotFoundError:
             not_found_error = "El archivo no fue encontrado."
             flash(not_found_error)
-            
+
         except ValueError:
             no_integer_error = "El número ingresado debe ser un número entero."
             flash(no_integer_error)
 
         else:
-            cipher_file = Encryption(file_name, bytes_for_key)
-            cipher_file()
-            flash(cipher_file)
+            cipher_file = encryption.AesEncryptor(file_name, bytes_for_key)
+            flash_message = str(cipher_file())
+            flash(flash_message)
 
     return render_template('cipher_page.html')
 
@@ -65,28 +67,29 @@ def decipher():
     private key used for decipher the AES encryption in the file. 
     '''
     if request.method == 'GET':
-      new_message = 'Recuerde cargar la llave privada correcta.'
-      flash(new_message)
-      
+        new_message = 'Recuerde cargar la llave privada correcta.'
+        flash(new_message)
+
     if request.method == 'POST':
         try:
             encrypted_file = request.form['cipher_file_path']
             key_file = request.form['private_key_path']
-            create_new = Constructor(encrypted_file, key_file)
-            path_to_key, path_to_encrypted_file = create_new.ReadKeyPath()
+            create_new = constructor.Constructor(encrypted_file, key_file)
+            path_to_key, path_to_encrypted_file = create_new.read_key_path()
+
             if not os.access(path_to_encrypted_file, os.F_OK):
                 raise FileNotFoundError
             if not os.access(path_to_key, os.F_OK):
                 raise FileNotFoundError
-        
+
         except FileNotFoundError:
             files_not_found_error = "El archivo cifrado o la llave privada no fueron encontrados."
             flash(files_not_found_error)
 
         else:
-            decipher_file = Decryption(encrypted_file, key_file)
-            decipher_file()
-            flash(decipher_file)
+            decipher_file = decryption.AesDecryptor(encrypted_file, key_file)
+            flash_message = str(decipher_file())
+            flash(flash_message)
 
     return render_template('decipher_page.html')
 
